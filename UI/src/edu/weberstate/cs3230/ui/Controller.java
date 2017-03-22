cc4672c9f12cff934b87cebe5e80d89b52f7897e
jdickey <josh.dickey1@gmail.com>
2017-03-22 10:54:47 -0600

    Added in UI- Controller, mainUI, and fxml

---
+++ UI/src/edu/weberstate/cs3230/ui/Controller.java
@@ -0,0 +1,7 @@
+package edu.weberstate.cs3230.ui;
+
+/**
+ * Created by jdickey on 3/22/2017.
+ */
+public class Controller {
+}
---
+++ UI/src/edu/weberstate/cs3230/ui/MainUI.java
@@ -0,0 +1,27 @@
+package edu.weberstate.cs3230.ui;
+/**
+ * Created by jdickey on 3/22/2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
