6014489083b8f37d4c10db07bfd6042f55be5e97
jDickey <joshuadickey@mail.weber.edu>
2017-04-09 16:09:18 -0600

    Add navigation to new scene

--- UI/src/edu/weberstate/cs3230/ui/SetupController.java
+++
@@ -1,7 +0,0 @@
-package edu.weberstate.cs3230.ui;
-
-/**
- * Created by joshd on 3/27/2017.
- */
-public class SetupController {
-}
--- UI/src/edu/weberstate/cs3230/ui/StartController.java
+++ UI/src/edu/weberstate/cs3230/ui/StartController.java
@@ -6,13 +6,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Logger;
 
 /**
  * Created by joshd on 3/27/2017.
@@ -21,23 +22,27 @@ public class StartController implements Initializable{
 
     @FXML
     private Button btnStart, btnQuit;
+    Parent root;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("setup.fxml"));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+//        try {
+//            root = FXMLLoader.load(getClass().getResource("layout/start.fxml"));
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
-    public void onStartButtonClicked(ActionEvent actionEvent) {
+    public void onStartButtonClicked(ActionEvent actionEvent) throws IOException {
+        Stage stage;
+        stage = (Stage) btnStart.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("layout/setup.fxml"));
 
-        Logger.getAnonymousLogger().fine("Start Button Clicked");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
 
     }
